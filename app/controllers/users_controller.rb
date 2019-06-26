63656e1bc0a3775778326c3e273e6e33b0cc318d
Jonathan Serafini <jonathan.serafini@gmail.com>
2019-06-26 16:28:26 +0200

    add events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def dashboard
     @activity = Activity.new
+    @event = Event.new
     @activities = Activity.all
+    @events = Event.all
   end
 end
