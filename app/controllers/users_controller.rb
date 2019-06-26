74ef8d49eba0d6ca5f5a1dbe5287d90bcee41b55
Jonathan Serafini <jonathan.serafini@gmail.com>
2019-06-26 17:29:08 +0200

    add schedule line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def dashboard
+    @batch = Batch.where(number: "#290")
     @activity = Activity.new
     @event = Event.new
     @activities = Activity.all
     @events = Event.all
+
+    @batch_events = Event.where(batch: @batch)
+    @batch_battles = Battle.where(batch: @batch)
+
+    @schedule = @batch_events + @batch_battles
+    @timeline_schedule = @schedule.sort_by(&:date)
+
   end
 end
--- app/models/battle.rb
+++ app/models/battle.rb
@@ -2,4 +2,8 @@ class Battle < ApplicationRecord
   belongs_to :batch
 
   has_many :choices
+
+  def title
+    "#{choices.first&.activity&.name} vs #{choices.second&.activity&.name}"
+  end
 end
